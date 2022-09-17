7a5dc385b629cd1edeff9ec2e57cb49a6b235518
bkantwi <bernardantwi2010@gmail.com>
2022-09-17 21:23:16 +0000

    removed image

--- main.py
+++ main.py
@@ -15,10 +15,14 @@ def main_screen():
     screen.geometry("375x398")
 
     #icon
-    image_icon=PhotoImage(file="keys.png")
-    screen.iconphoto(False,image_icon)
+    #image_icon=PhotoImage(file="image.jpg")
+    #screen.iconphoto(False,image_icon)
 
     screen.title("Bkantwi Hashing")
+    
+    Label(text="Value for encryption and decryption", fg="balck", font=("calibri",13)).place(x=10,y=10)
+    
+    text1 = Text(font="Robote 20", bg="white", relief=GROOVE,wrap=WORD, bd=0)
 
     screen.mainloop()
     
